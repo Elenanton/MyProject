708e1536451113cd83933a6937434ae8891fd5eb
Elena Antonakou <helena.antonakou@gmail.com>
2022-01-09 19:54:44 +0200

    numbers

--- test.py
+++ test.py
@@ -3,3 +3,12 @@ anything = input()
 print("Hmm..", anything ,"Tell me something else.." )
 anything = input()
 print("Hmm...",anything, "Ok")
+
+number = int(input("Enter a number:"))
+if number >10:
+    print("You entered a number larger than ten!")
+else:
+    print("You entered a number smaller than ten!") 
+
+    
+
